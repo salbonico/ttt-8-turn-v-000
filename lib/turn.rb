286f9cee4a928e4015d5998a04804959e6e787d4
Steve Albonico <salbonico@gmail.com>
2019-01-11 22:18:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,3 +4,20 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index <0 || index >8
+    return false
+  elsif board[index] == " "
+    return true
+  else return false
+  end
+end
+
+def move(board, input, xo="X")
+  index = input.to_i
+  index = index -1
+board[index] = xo
+end
+
